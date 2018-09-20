8db44a314888988e807abfb525718e980f9e2f9c
Han Lin Aung <hanlinaunghack@gmail.com>
2018-09-20 09:59:34 -0700

    finished linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,22 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node (value, null, list.tail);
+    if (list.tail) list.tail.next = newNode;
+    else list.head = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+  	if (!list.head) return null;
+    let value = list.head.value;
+    list.head = list.head.next;
+  	if (list.head) list.head.previous = null;
+  	else list.tail = null;
+    return value;
   };
 
   list.contains = function(target) {
+    let currentNode = list.head;
+    while(currentNode) {
+      if(currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value, next, previous) {
   var node = {};
 
   node.value = value;
-  node.next = null;
+  node.next = next;
+  node.previous = previous;
 
   return node;
 };
